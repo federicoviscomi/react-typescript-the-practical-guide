b2862afdea0147063a959d4b76e93d00b629fa16
federicoviscomi <federicoviscomi@gmail.com>
2024-10-06 19:32:14 +0100

    49

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from './CourseGoal.tsx';
 import { type CourseGoal as CGoal } from '../App.tsx';
+import InfoBox from './InfoBox.tsx';
+import { ReactNode } from 'react';
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -10,15 +12,27 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+
+  const renderInfoBox = () => {
+    if (goals.length === 0) {
+      return <InfoBox mode="hint">No couse goals eyet</InfoBox>;
+    }
+    if (goals.length >= 4) {
+      return <InfoBox mode="warning" severity='high'>Too many goals</InfoBox>
+    }
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {renderInfoBox()}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
