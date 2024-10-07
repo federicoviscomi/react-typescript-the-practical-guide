cf3db5be226cdfcee3df23b9ec76d42246e0bc50
fevi_1spatial <federico.viscomi@1spatial.com>
2024-10-07 09:33:52 +0100

    54

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <main>
       <p>
-        <Button el="button">A button</Button>
+        <Button>A button</Button>
       </p>
       <p>
-        <Button el="anchor" href="https://google.com">A link</Button>
+        <Button href="https://google.com">A link</Button>
       </p>
       <Input id="name" label="insert name" type="text"></Input>
       <Input id="age" label="age here" type="number"></Input>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { FC } from "react";
 
-type ButtonProps = {
-    el: 'button';
-} & ComponentPropsWithoutRef<'button'>
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+    href?: never;
+};
 
-type AnchorProps = {
-    el: 'anchor';
-} & ComponentPropsWithoutRef<'a'>
+type AnchorProps = ComponentPropsWithoutRef<'a'> & {
+    href?: string;
+}
+
+const isAnchorProps = (props: ButtonProps | AnchorProps): props is AnchorProps => {
+    return 'href' in props;
+}
 
 const Button: FC<ButtonProps | AnchorProps> = (props: ButtonProps | AnchorProps) => {
-    if (props.el === 'anchor') {
+    if (isAnchorProps(props)) {
         return <a className='button' {...props}></a>;
     }
     return <button className='button' {...props}></button>;
