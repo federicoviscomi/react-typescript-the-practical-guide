4b462d4bd85a3133b43350db91ed2cba7707254e
federicoviscomi <federicoviscomi@gmail.com>
2024-10-08 22:17:31 +0100

    61

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Input from './components/Input.tsx';
-import Form from './components/Form.tsx';
+import Form, { FormHandle } from './components/Form.tsx';
 import Button from './components/Button.tsx';
+import { useRef } from 'react';
 
 export default function App() {
+  const customForm = useRef<FormHandle>(null);
+
   const handleSave = (data: unknown) => {
     const extractedData = data as {name: string, age: number};
-
+    customForm.current?.clear();
   };
+  
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name">
         </Input>
         <Input type="number" label="Age" id="age">
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,24 @@
-import { ComponentPropsWithoutRef, FC, FormEvent } from 'react';
+import { ComponentPropsWithoutRef, FormEvent, forwardRef, useImperativeHandle, useRef } from 'react';
+
+export type FormHandle = {
+    clear: () => void;
+}; 
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
-    onSave: (value: unknown) => void; 
+    onSave: (value: unknown) => void;
 };
 
-const Form: FC<FormProps> = ({onSave, children, ...props}) => {
+const Form = forwardRef<FormHandle, FormProps>(function ({ onSave, children, ...props }, ref) {
+    const form = useRef<HTMLFormElement>(null);
+
+    useImperativeHandle(ref, () => {
+        return {
+            clear() {
+                form.current?.reset();
+            }
+        };
+    });
+
     function handleSubimt(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
@@ -12,10 +26,10 @@ const Form: FC<FormProps> = ({onSave, children, ...props}) => {
         onSave(data);
     };
     return (
-        <form onSubmit={handleSubimt} {...props}>
+        <form onSubmit={handleSubimt} {...props} ref={form}>
             {children}
         </form>
     );
-}
+});
 
 export default Form;
