87dcd6f7bbadb6137188828fe408e9ee1eeb82ab
federicoviscomi <federicoviscomi@gmail.com>
2024-10-08 21:43:19 +0100

    58

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import Input from './components/Input.tsx';
-import Button from './components/Button.tsx';
-import Container from './components/Container.tsx';
+import { useRef } from 'react';
 
 export default function App() {
+  const input = useRef<HTMLInputElement>(null);
+  
   return (
     <main>
-      <p>
-        <Button>A button</Button>
-      </p>
-      <p>
-        <Button href="https://google.com">A link</Button>
-      </p>
-      <Input id="name" label="insert name" type="text"></Input>
-      <Input id="age" label="age here" type="number"></Input>
-      <Container as={Button} onClick={() => {}} type="button">Cliccami</Container>
+      <Input label="test" id="test" ref={input}>
+      </Input>
     </main>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,17 @@
-import { ComponentPropsWithoutRef, FC } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
     id: string;
     label: string;
 } & ComponentPropsWithoutRef<'input'>;
 
-const Input: FC<InputProps> = ({ id, label, ...props }) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(function ({ id, label, ...props }, ref) {
     return (
         <p>
             <label htmlFor={id}>{label}</label>
-            <input id={id} {...props}></input>
+            <input id={id} {...props} ref={ref}></input>
         </p>
     );
-}
+});
 
 export default Input;
