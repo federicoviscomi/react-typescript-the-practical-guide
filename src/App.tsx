ff829a1521d48e0f8643d9d73b502a7a2fceb06a
fevi_1spatial <federico.viscomi@1spatial.com>
2024-10-09 16:18:52 +0100

    80

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,53 @@
-import AddTimer from "./components/AddTimer.tsx";
-import Header from "./components/Header.tsx";
-import Timers from "./components/Timers.tsx";
-import TimersContextProvider from "./store/timers-context";
+import { type ReactNode, useEffect, useState } from 'react';
+
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
+import { get } from './util/http.ts';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
   return (
-    <>
-      <TimersContextProvider>
-        <Header />
-        <main>
-          <AddTimer />
-          <Timers />
-        </main>
-      </TimersContextProvider>
-    </>
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fetching process."
+      />
+      {content}
+    </main>
   );
 }
 
